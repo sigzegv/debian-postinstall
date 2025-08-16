ee9dac8548d453f7798b112704c91a30c8bb057a
Cavern <6304596+sigzegv@users.noreply.github.com>
2025-08-17 00:40:08 +0200

    gnome post install

---
+++ debian-13-trixie/post-install-gnome.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## gnome section
+# packages
+sudo apt install -y nautilus-share
+sudo apt install -y gnome-shell-extension-user-theme gnome-shell-extension-appindicator
+
+# default settings
+gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
+gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
+gsettings set org.gnome.desktop.calendar show-weekdate true
+gsettings set org.gnome.mutter center-new-windows true
+gsettings set org.gnome.desktop.interface cursor-theme Graphite-dark-cursors
+gsettings set org.gnome.mutter check-alive-timeout 30000
+
+# cleaning
+sudo apt remove -y gnome-shell-extension-prefs
+sudo apt autoremove -y
--- debian-13-trixie/post-install.sh
+++ debian-13-trixie/post-install.sh
@@ -9,7 +9,7 @@ sudo apt update && sudo apt dist-upgrade -y
 
 # install useful packages
 sudo apt install -y g++ cmake pkg-config libfontconfig-dev git curl apt-file lua5.4
-sudo apt install -y gdebi xsel libsass1 sassc nautilus-share dnsmasq
+sudo apt install -y gdebi xsel libsass1 sassc dnsmasq
 sudo apt-file update
 
 echo "vm.max_map_count=2147483642" | sudo tee -a /etc/sysctl.conf > /dev/null
@@ -18,21 +18,3 @@ sudo sed -i -e 's/quiet/quiet splash/g' /etc/default/grub
 # a nice cursor for system
 cd /usr/local/share
 sudo git clone https://github.com/vinceliuice/Graphite-cursors && cd Graphite-cursors && sudo ./install.sh
-
-## gnome section
-# packages
-sudo apt install -y gnome-shell-extension-user-theme gnome-shell-extension-appindicator
-
-# default settings
-gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
-gsettings set org.gnome.desktop.interface clock-show-weekday true
-gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
-gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
-gsettings set org.gnome.desktop.calendar show-weekdate true
-gsettings set org.gnome.mutter center-new-windows true
-gsettings set org.gnome.desktop.interface cursor-theme Graphite-dark-cursors
-gsettings set org.gnome.mutter check-alive-timeout 30000
-
-# cleaning
-sudo apt remove -y gnome-shell-extension-prefs
-sudo apt autoremove -y
